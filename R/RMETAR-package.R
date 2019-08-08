a9b4e3dabfd4aadff6758331d7b6c120e3cba1a4
Andrie <apdevries@gmail.com>
2019-08-08 09:44:34 +0100

    Add package level documentation

--- R/RMETAR-package.R
+++ R/RMETAR-package.R
@@ -1,5 +1,12 @@
+#' @keywords internal
 #' @useDynLib RMETAR, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 #' @importFrom fasttime fastPOSIXct 
 #' @importFrom utils read.csv
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
