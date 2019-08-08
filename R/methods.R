5fe8862d7ca0f242aa413d54f5fba6580ee06b16
Jan Wijffels <jwijffels@bnosac.be>
2019-08-08 12:22:53 +0200

    add print.decodedMETAR

--- R/methods.R
+++ R/methods.R
@@ -2,3 +2,10 @@
 str.decodedMETAR <- function(x, list.len = 500, ...){
   NextMethod("str", x, list.len = list.len, ...)
 }
+
+
+
+#' @export
+print.decodedMETAR <- function(x, ...){
+  cat(x$printout)
+}
